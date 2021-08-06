71933cec9b279d0a4d4f7e3d0ab464753e6f7f6f
Breno Farias <87508080+BrenoFar@users.noreply.github.com>
2021-08-06 15:45:45 -0300

    Update combsort python english.py

--- combsort python english.py
+++ combsort python english.py
@@ -2,6 +2,7 @@ from random import randrange
 def combsort(num):                 #Defining combsort as a function
     gap = len(num)
     swaps=True
+    troca=0
     while gap > 1 or swaps:
         gap = max(1, int(gap / 1.25))  # minimum gap is 1
         swaps=False                    #Turning off just to verify if had the swap
@@ -9,10 +10,21 @@ def combsort(num):                 #Defining combsort as a function
             j = i+gap
             if num[i] > num[j]:
                 num[i], num[j] = num[j], num[i]
-                swaps=True           
+                swaps=True
+                if swaps:
+                    troca=troca+1                      #counting the swaps
+    print('Houve {0} troca(as)'.format(troca))         #telling to the user how much swaps happened
+
 
  
 num_list = [75, 16, 55, 19, 48, 14, 2, 61, 22, 100]     #A pre-made list with 10 indexes
+#listateste= [127,147,50,149,101] lista desordenada            #MANUALLY TESTING A LIST
+#listateste= [101,147,50,149,127] intervalo como 4 V 1 troca   #GAP=4
+#listateste= [101,127,50,149,147] intervalo como 3 V 1 troca   #GAP=3
+#listateste= [50,127,101,149,147] intervalo como 2 V 1 troca   #GAP=2
+#listateste= [50,101,127,147,149] intervalo como 1 V 2 troca   #GAP=1
+#listateste= [50,101,127,147,149] lista ordenada
+
 lista = []                                              #Empty list that we add some values
 print('Choose one of those options!')
 print('1- Pre-made list')
@@ -27,7 +39,6 @@ if P1 ==1 or P1 ==2 or P1==3:
         print('Before: ',num_list)                                              #showing the before not sorted
         combsort(num_list)                                                      #calling the combsort now as a function apllying in the pre-made list "num_list"
         print('After: ',num_list)                                               #showing the list after being sorted
-
 
     elif P1==2:
         aux = int(input('Define your list length (ex: 1/10/50/30/100) \n'))
